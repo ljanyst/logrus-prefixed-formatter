42c8339fdda3d52f9019540bcf354682c929dff2
Lukasz Janyst <lukasz@jany.st>
2024-02-20 21:07:34 +0100

    Add context example

---
+++ examples/context/context.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	prefixed "github.com/ljanyst/logrus-prefixed-formatter"
+	"github.com/sirupsen/logrus"
+)
+
+type TestContext struct {
+	Type string
+	Foo  string
+	Bar  int
+}
+
+func (t *TestContext) String() string {
+	return fmt.Sprintf("%-5s f: %-10s b: %3d", t.Type, t.Foo, t.Bar)
+}
+
+func init() {
+	formatter := new(prefixed.TextFormatter)
+	formatter.MinPrefixWidth = 25
+	logrus.SetFormatter(formatter)
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+func main() {
+	logrus.Info("Before context")
+	ctx := &TestContext{}
+	log := prefixed.Log(context.WithValue(context.Background(), "logging-context", ctx))
+	log.Debugf("Started observing beach")
+	ctx.Type = "foo"
+	log.Debugf("A group of walrus emerges from the ocean")
+	ctx.Foo = "bar"
+	log.Warnf("The group's number increased tremendously!")
+	ctx.Bar = 42
+	log.Infof("Temperature changes")
+}
